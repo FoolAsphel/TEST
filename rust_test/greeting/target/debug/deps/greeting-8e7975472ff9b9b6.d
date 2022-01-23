cd89b6b5f3f813172fc1813471e71ecdda997bc1
FoolAsphel <foolasphel@outlook.com>
2022-01-23 23:46:50 +0800

    update

---
+++ rust_test/greeting/target/debug/deps/greeting-8e7975472ff9b9b6.d
@@ -0,0 +1,5 @@
+d:\Code\rust_test\greeting\target\debug\deps\greeting-8e7975472ff9b9b6.rmeta: src\main.rs
+
+d:\Code\rust_test\greeting\target\debug\deps\greeting-8e7975472ff9b9b6.d: src\main.rs
+
+src\main.rs:
---
+++ rust_test/greeting/target/debug/deps/greeting-fbfe9392db2daf2a.d
@@ -0,0 +1,5 @@
+d:\Code\rust_test\greeting\target\debug\deps\greeting-fbfe9392db2daf2a.rmeta: src\main.rs
+
+d:\Code\rust_test\greeting\target\debug\deps\greeting-fbfe9392db2daf2a.d: src\main.rs
+
+src\main.rs:
