8e99594bd21e0105b0a561c513debd7239306de6
FoolAsphel <foolasphel@outlook.com>
2022-01-23 20:49:26 +0800

    update

---
+++ rust_test/greeting/target/debug/deps/greeting.d
@@ -0,0 +1,5 @@
+D:\Code\rust_test\greeting\target\debug\deps\greeting.exe: src\main.rs
+
+D:\Code\rust_test\greeting\target\debug\deps\greeting.d: src\main.rs
+
+src\main.rs:
---
+++ rust_test/greeting/target/debug/greeting.d
@@ -0,0 +1,1 @@
+D:\Code\rust_test\greeting\target\debug\greeting.exe: D:\Code\rust_test\greeting\src\main.rs
